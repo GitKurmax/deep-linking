d6fba08f6ac514e9f463aef047140d61d3be48c6
alexander.kurmoiarov <alexandrmail32@gmail.com>
2024-12-26 22:10:26 +0200

    Added deep linking for ios

--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -3,18 +3,25 @@ import SecondScreen from './SecondScreen.tsx';
 import {Stack} from '../navigation/RootStack.tsx';
 import FirstScreen from './FirstScreen.tsx';
 
-
 const HomeScreen: React.FC<any> = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="First" component={FirstScreen} options={{
-                headerShown: false,
-            }}/>
-            <Stack.Screen name="Second" component={SecondScreen} options={{
-                headerShown: false,
-            }}/>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="First"
+        component={FirstScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Second"
+        component={SecondScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default HomeScreen;
